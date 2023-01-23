628993531501b57ce843d507c06e99ca1df84256
Tom Granberg <tom.andre.granberg@gmail.com>
2023-01-23 13:10:29 +0100

    Initial Commit AugFT22_CA_JSS

---
+++ jss-ca-1/routes/meme.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+// const { resolve } = require('path');
+const axios = require('axios');
+
+router.get('/', function (req, res, next) {});
+
+module.exports = router;
---
+++ jss-ca-1/routes/memes.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+// const { resolve } = require('path');
+const axios = require('axios');
+
+//BM New API endpoint to use for the data from the soccer API
+router.get('/', function (req, res, next) {});
+
+module.exports = router;
